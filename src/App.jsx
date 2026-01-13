6772ce8564d4cd06f07b7ed9e90582ae0a6c7b31
Viniciuslvargas <vinilvargas3112@gmail.com>
2026-01-12 22:03:36 -0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from './components/Banner/Banner'
+import Formulario from './components/Formulario'
+import './index.css'
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Banner />
+        <Formulario />
+      </div>
+     </>
+  )
+}
+
+export default App
