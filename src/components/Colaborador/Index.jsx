6dfc2cabc90a5927ae7f71893f2a553a7fc21bc0
Viniciuslvargas <vinilvargas3112@gmail.com>
2026-01-24 22:18:16 -0300

    last update

--- src/components/Colaborador/Index.jsx
+++ src/components/Colaborador/Index.jsx
@@ -1,4 +1,4 @@
-import './colaborador.css'
+import './Colaborador.css'
 const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
         return (<div className='colaborador'>
             <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
